0daf243af446d7ec5f59c450a8a387d3616bf38b
Joscha Henningsen <joscha.henningsen@tum.de>
2021-01-04 09:23:34 +0100

    cache view page for one day & add comments

--- api/CacheAccess.php
+++ api/CacheAccess.php
@@ -2,12 +2,19 @@
 
 class CacheAccess
 {
-    static function getHtml($url){
+
+    /**
+     * Loads html from url or cache, if present.
+     * @param $url
+     * @param int $ttl
+     * @return false|file_get_html|mixed
+     */
+    static function getHtml($url, $ttl = 60){
         if(apcu_exists($url)){
             $html = apcu_fetch($url);
         }else{
             $html = file_get_html($url);
-            apcu_add($url, $html, 60);
+            apcu_add($url, $html, $ttl);
         }
         return $html;
     }
