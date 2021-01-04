6b942d16ea779c1e2bc01626d7b39d09a1c9df29
Joscha Henningsen <44805696+joschahenningsen@users.noreply.github.com>
2021-01-04 16:43:35 +0100

    Update get_rbg_hls_link.php

--- api/get_rbg_hls_link.php
+++ api/get_rbg_hls_link.php
@@ -3,7 +3,7 @@ include 'simple_html_dom.php';
 include_once 'CacheAccess.php';
 
 function get_rbg_hls_link(string $url): ?string {
-  $html = CacheAccess::getHtml($url, 60, 60*60*24); // this site should be static, therefore we cache it longer.
+  $html = CacheAccess::getHtml($url, 60*60*24); // this site should be static, therefore we cache it longer.
 
   if($html) {
     $matches = array();
