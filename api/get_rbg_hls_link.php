c3f4abc59f44459c80daebe2ece2fdd87a740234
Joscha Henningsen <joscha.henningsen@tum.de>
2021-01-12 08:17:16 +0100

    Cache hls link as well

--- api/get_rbg_hls_link.php
+++ api/get_rbg_hls_link.php
@@ -1,9 +1,14 @@
 <?php
 include 'simple_html_dom.php';
-include_once 'ShortLinkAccess.php';
 
 function get_rbg_hls_link(string $url): ?string {
-    $html = ShortLinkAccess::getHtml($url, 60 * 60 * 24); // this site should be static, therefore we cache it longer.
+    $CACHEKEY = "get_rbg_hls_link_" . $url;
+    $CACHETTL = 60 * 60 * 24; // One day
+    if (apcu_exists($CACHEKEY)) {
+        return apcu_fetch($CACHEKEY);
+    }
+
+    $html = file_get_html($url);
 
     if ($html) {
         $matches = [];
@@ -11,10 +16,11 @@ function get_rbg_hls_link(string $url): ?string {
         preg_match('/MMstartStream.*\'(.+)\'/', $html, $matches);
 
         if (sizeof($matches) >= 1) {
+            apcu_add($CACHEKEY, $matches[0], $CACHETTL);
             return $matches[1];
         }
     }
-
+    apcu_add($CACHEKEY, null, $CACHETTL);
     return null;
 }
 
