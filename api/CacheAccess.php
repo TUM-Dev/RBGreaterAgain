d16abb452e7ffa2668c86ed5bdd72f6f7e1a4fb5
Joscha Henningsen <joscha.henningsen@tum.de>
2021-01-12 07:45:32 +0100

    stop caching html, cache result instead.

--- api/CacheAccess.php
+++ api/CacheAccess.php
@@ -1,25 +1,7 @@
 <?php
 
 class CacheAccess {
-    const SHORTLINKPREFIXHTML = "shortlink_to_html_";
     const SHORTLINKPREFIXURL = "shortlink_to_url_";
-
-    /**
-     * Loads html from url or cache, if present.
-     * @param $url
-     * @param int $ttl
-     * @return false|file_get_html|mixed
-     */
-    static function getHtml($url, $ttl = 60) {
-        $shortLink = self::SHORTLINKPREFIXHTML . self::getVideoShortId($url); // get and store the shortlink in the cache
-        if (apcu_exists($shortLink)) {
-            $html = apcu_fetch($shortLink);
-        } else {
-            $html = file_get_html($url);
-            apcu_add($shortLink, $html, $ttl);
-        }
-        return $html;
-    }
 
     /**
      * returns a video shortId from cache if present, otherwise generates one
--- api/get_rbg_stream_site.php
+++ api/get_rbg_stream_site.php
@@ -2,25 +2,31 @@
 include 'simple_html_dom.php';
 include_once 'CacheAccess.php';
 
+
 function ParseInformation($link) {
-    // First get html of remote page
-    $html = CacheAccess::getHtml($link);
+    $CACHEKEY = "get_rbg_stream_site";
+    if (apcu_exists($CACHEKEY)) {
+        return apcu_fetch($CACHEKEY);
+    } else {
+        // First get html of remote page
+        $html = file_get_html($link);
 
-    $veranstaltung = $html->find("main", 1)->find("font", 0)->find("b", 0)->innertext;
-    $date = $html->find("main", 1)->find("font", 0)->find("b", 1)->innertext;
-    $time = $html->find("main", 1)->find("font", 0)->find("b", 2)->innertext;
+        $veranstaltung = $html->find("main", 1)->find("font", 0)->find("b", 0)->innertext;
+        $date = $html->find("main", 1)->find("font", 0)->find("b", 1)->innertext;
+        $time = $html->find("main", 1)->find("font", 0)->find("b", 2)->innertext;
 
-    $link;
-    if ($html) {
-        $matches = [];
+        $link;
+        if ($html) {
+            $matches = [];
 
-        preg_match('/MMstartStream.*\'(.+)\'/', $html, $matches);
+            preg_match('/MMstartStream.*\'(.+)\'/', $html, $matches);
 
-        if (sizeof($matches) >= 1) {
-            $link = $matches[1];
+            if (sizeof($matches) >= 1) {
+                $link = $matches[1];
+            }
         }
+        $parsedData = ["name" => $veranstaltung, "date" => $date, "time" => $time, "hls_url" => $link];
+        apcu_add($CACHEKEY, $parsedData);
+        return $parsedData;
     }
-
-
-    return ["name" => $veranstaltung, "date" => $date, "time" => $time, "hls_url" => $link];
 }
